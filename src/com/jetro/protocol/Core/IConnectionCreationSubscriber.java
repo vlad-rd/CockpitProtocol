583e6b67a0310091359b95d8ea120144b0dfdf27
ranhaveshushwork <ran.haveshush.work@gmail.com>
2014-12-08 14:48:22 +0200

    IConnectionCreationSubscriber: added to support error messages to the user;

---
+++ src/com/jetro/protocol/Core/IConnectionCreationSubscriber.java
@@ -0,0 +1,5 @@
+package com.jetro.protocol.Core;
+
+public interface IConnectionCreationSubscriber {
+	void ConnectionCreated(boolean result, String message) ;
+}
---
+++ src/com/jetro/protocol/Core/ICreateCallback.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.jetro.protocol.Core;
+
+/**
+ * @author ran.h
+ *
+ */
+public interface ICreateCallback {
+	
+	public void OnError(Exception error);
+
+}
